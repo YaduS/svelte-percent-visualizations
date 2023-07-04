e6c0e065c48391d4731a3ad0125e53e580be6738
Yadu S <yadusasankan@gmail.com>
2023-07-04 14:07:18 +0530

    feat: add base './' to vite confg to facilitate deployment using github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: './', // so that it uses relative path in the generated html => so that it works with the hacky github deployment that im doing now
   build: {
     outDir: './docs', // relative to index.html
     // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
