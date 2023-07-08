ca536b7620749180daf0abb33557fd3cbf7827fc
Yadu S <yadusasankan@gmail.com>
2023-07-08 18:55:47 +0530

    feat: add peripheral elements - xAxis yAxis etc.

---
+++ src/app/components/scatterplot/AxisY.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let height;
+  export let width;
+  export let yScale;
+  export let margin;
+  const yTick = [0, 50, 100, 150, 200]; // todo: use d3.range or some such operator to generate appropriate ticks, currently it not synced..
+</script>
+
+<g transform="translate({margin.left} {margin.top})">
+  {#each yTick as tick}
+    <text x={0} y={yScale(tick)} dy="-6">{tick}%</text>
+    <line
+      x1="0"
+      y1={yScale(tick)}
+      x2={width}
+      y2={yScale(tick)}
+      stroke={tick === 0 ? 'black' : '#cecece'}
+    />
+  {/each}
+</g>
+
+<style></style>
--- src/app/components/scatterplot/Scatterplot.tab.svelte
+++ src/app/components/scatterplot/Scatterplot.tab.svelte
@@ -3,8 +3,12 @@
   import { scaleLinear } from 'd3-scale';
   import { tableData } from '../../services/data.service';
   import { calcPercent } from '../../services/util';
+  import AxisX from './AxisX.svelte';
+  import AxisY from './AxisY.svelte';
+
   let width = 400;
   let height = 400;
+  const margin = { top: 30, right: 70, bottom: 50, left: 0 };
 
   // few reactive declarations for calculating maxHours and maxPercent
   $: maxHoursStudied = max($tableData, (e) => e.hoursStudied * 1.1); // adding a 10% right margin by multiplying with 1.1;
@@ -14,21 +18,29 @@
     else return 0;
   });
 
-  $: xScale = scaleLinear().domain([0, maxHoursStudied]).range([0, width]);
-  $: yScale = scaleLinear().domain([0, maxPercentage]).range([height, 0]);
+  $: xScale = scaleLinear()
+    .domain([0, maxHoursStudied])
+    .range([0, width - margin.left - margin.right]);
+  $: yScale = scaleLinear()
+    .domain([0, maxPercentage])
+    .range([height - margin.top - margin.bottom, 0]);
 </script>
 
 <svg {width} {height}>
-  {#each $tableData as row}
-    <circle
-      cx={xScale(row.hoursStudied)}
-      cy={yScale(calcPercent(row.score, row.total))}
-      r="8"
-      fill="purple"
-      stroke="black"
-    />
-    <!-- todo: what is the way where we dont have to run calcPercent every time? --ys -->
-  {/each}
+  <AxisX {height} {xScale} {width} {margin} />
+  <AxisY {yScale} {height} {width} {margin} />
+  <g class="circles" transform="translate({margin.left} {margin.top})">
+    {#each $tableData as row}
+      <circle
+        cx={xScale(row.hoursStudied)}
+        cy={yScale(calcPercent(row.score, row.total))}
+        r="8"
+        fill="purple"
+        stroke="black"
+      />
+      <!-- todo: what is the way where we dont have to run calcPercent every time? --ys -->
+    {/each}
+  </g>
 </svg>
 
 <style lang="scss"></style>
